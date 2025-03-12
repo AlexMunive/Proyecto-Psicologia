5faaf26605c13fa1af9cd1f2cd1fa8f9d7abbf1c
ALEX <alexmunive.0213@gmail.com>
2025-03-12 18:13:57 -0500

    avance proyecto psicologia

---
+++ src/components/gallery.jsx
@@ -0,0 +1,35 @@
+import { Image } from "./image";
+import React from "react";
+
+export const Gallery = (props) => {
+  return (
+    <div id="portfolio" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Historial</h2>
+          <p>
+          "A lo largo de mi carrera como psicólogo, he acompañado a muchas personas en su camino hacia el bienestar emocional. Aquí comparto algunos de los casos más significativos, siempre con respeto y confidencialidad."
+          </p>
+        </div>
+        <div className="row">
+          <div className="portfolio-items">
+            {props.data
+              ? props.data.map((d, i) => (
+                  <div
+                    key={`${d.title}-${i}`}
+                    className="col-sm-6 col-md-4 col-lg-4"
+                  >
+                    <Image
+                      title={d.title}
+                      largeImage={d.largeImage}
+                      smallImage={d.smallImage}
+                    />
+                  </div>
+                ))
+              : "Loading..."}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/navigation.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import Logo1 from "../../public/img/principal/logo1.jpg"
+import Logo2 from "../../public/img/principal/logo2.jpg"
+
+export const Navigation = (props) => {
+  return (
+    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
+          >
+            {" "}
+            <span className="sr-only">Toggle navigation</span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+          </button>
+          <a className="navbar-brand page-scroll" href="#page-top">
+            <div style={{display: "flex", flexDirection: "row", marginTop: "-20px"}}>
+
+          <img src={Logo1} style={{width: "50px", marginRight: "10px"}}></img>
+          <img src={Logo2} style={{width: "50px"}}></img>
+            </div>
+          </a>{" "}
+        </div>
+
+        <div
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
+        >
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="#features" className="page-scroll">
+                Inicio
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="page-scroll">
+                Sobre mí
+              </a>
+            </li>
+            <li>
+              <a href="#services" className="page-scroll">
+                Servicios
+              </a>
+            </li>
+            <li>
+              <a href="#portfolio" className="page-scroll">
+                Historial
+              </a>
+            </li>
+            <li>
+              <a href="#testimonials" className="page-scroll">
+                Testimonios
+              </a>
+            </li>
+            {/* <li>
+              <a href="#team" className="page-scroll">
+                Team
+              </a>
+            </li> */}
+            <li>
+              <a href="#contact" className="page-scroll">
+                Contacto
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
